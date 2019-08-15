ba0955b4e9627066e98dd577ba4aa8b8f334a37c
LF6987 <39294180+LF6987@users.noreply.github.com>
2019-08-15 05:46:11 -0700

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 
 // TODO: Enter the path to your service account json file
 // Need help with this step go here: https://firebase.google.com/docs/admin/setup
-// const serviceAccount = require("C:\M19\ebowlgaming-firebase-adminsdk-00zno-8725a28fef.json");
+// const serviceAccount = require("M19/ebowlgaming-firebase-adminsdk-00zno-8725a28fef.json");
 
 // TODO: Enter your database url from firebase
 // admin.initializeApp({
